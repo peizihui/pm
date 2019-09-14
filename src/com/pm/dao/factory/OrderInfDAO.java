d0455ffd62ef468ce73e7b24f6f6b7451b3da38c
OranggeA <q1951141381@gmail.com>
2019-09-14 21:52:24 +0800

    添加-管理员-订单查看2.1

--- src/com/pm/dao/factory/OrderInfDAO.java
+++ src/com/pm/dao/factory/OrderInfDAO.java
@@ -1,12 +1,23 @@
 package com.pm.dao.factory;
 
+import com.pm.dao.datasource.VOrderinf;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class OrderInfDAO {
     private Session session;
+
     public OrderInfDAO(Session session){this.session = session;}
 
-    public List<OrderInf>
+    public List<VOrderinf> getAllOrderInf(){
+        Query<VOrderinf> query = session.createQuery("from  VOrderinf ",VOrderinf.class);
+        return query.getResultList();
+    }
+    public VOrderinf getOrderByOId(int OId){
+        Query query = session.createQuery("from VOrderinf where oId = ?1");
+        query.setParameter(1,OId);
+        return (VOrderinf)query.uniqueResult();
+    }
 }
