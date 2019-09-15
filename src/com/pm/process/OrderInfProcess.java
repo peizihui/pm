c27a3a2d04237e54f07ed82632e666ea99778353
lhy <544148096@qq.com>
2019-09-15 23:44:01 +0800

    添加查询视图的两个方法（一个通过订单ID，一个通过用户ID）

--- src/com/pm/process/OrderInfProcess.java
+++ src/com/pm/process/OrderInfProcess.java
@@ -1,6 +1,7 @@
 package com.pm.process;
 
 import com.pm.dao.datasource.VOrderinf;
+import com.pm.dao.datasource.VOrderinfId;
 import com.pm.dao.factory.OrderInfDAO;
 import com.pm.util.HibernateUtils;
 import org.hibernate.Session;
@@ -23,7 +24,17 @@ public class OrderInfProcess {
             return  null;
         }
     }
+
+
     public VOrderinf getOrderInfByOId(int OId){
         return orderInfDAO.getOrderByOId(OId);
+    }
+    public VOrderinfId getOrderInfByOId1(int OId){
+        return orderInfDAO.getOrderByOId1(OId);
+    }
+    public List<VOrderinfId> getOrderInfByUId(int uId){
+        return  orderInfDAO.getOrderByUId(uId);
+
+
     }
 }
