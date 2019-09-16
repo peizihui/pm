0616ad839d2e4b0c212e0be803a72b38ddb4a648
Smallis_Service <smallis2018@gmail.com>
2019-09-16 16:32:23 +0800

    增加-获取订单时间

--- src/com/pm/util/Tools.java
+++ src/com/pm/util/Tools.java
@@ -1,5 +1,8 @@
 package com.pm.util;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.regex.Pattern;
 
 public class Tools {
@@ -8,5 +11,18 @@ public class Tools {
     public boolean isALLIntger(String str){
         Pattern pattern = Pattern.compile("[0-9]*");
         return pattern.matcher(str).matches();
+    }
+
+    public Date getNowDate() {
+        //获取系统时间
+        java.util.Date uDate = new java.util.Date();
+        //设置时间格式
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return new java.sql.Date(sdf.parse(sdf.format(uDate)).getTime());
+        }catch (ParseException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
