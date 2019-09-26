8870bd5718a322788011ae93f33cc7b09608617c
Smallis_Service <smallis2018@gmail.com>
2019-09-27 00:30:51 +0800

    增加商品管理列表分页功能

--- src/com/pm/ui/manager/MGoods.java
+++ src/com/pm/ui/manager/MGoods.java
@@ -2,6 +2,7 @@ package com.pm.ui.manager;
 
 import com.pm.dao.datasource.Goods;
 import com.pm.process.GoodsProcess;
+import com.pm.util.Page;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -13,22 +14,36 @@ import java.util.Vector;
 public class MGoods {
     private JPanel jp1;
     private JPanel jp2;
+    private JPanel jp3;
     private JTable table;
     private JButton addGoodsButton;
     private JButton editGoodsButton;
     private JButton deleGoodsButton;
     private JButton refreshButton;
+    private JButton fristPageButton;
+    private JButton nextPageButton;
+    private JButton previousPageButton;
+    private JButton lastPageButton;
     private JFrame mainFrame;
-    private Object[][] data;
+    private int currentPage;
+    private int fristPage;
+    private int lastPage;
+    private int tableRows;
+    private List<Goods> goodsList;
 
     public MGoods() {
         addGoodsButton = new JButton("上架商品");
         editGoodsButton = new JButton("修改商品信息");
         deleGoodsButton = new JButton("删除商品");
         refreshButton = new JButton("刷新");
+        fristPageButton = new JButton("首页");
+        previousPageButton = new JButton("上一页");
+        nextPageButton = new JButton("下一页");
+        lastPageButton = new JButton("末页");
 
         jp1 = new JPanel();
         jp2 = new JPanel();
+        jp3 = new JPanel();
 
 
         //设置表格
@@ -44,11 +59,13 @@ public class MGoods {
         table.setModel(new DefaultTableModel(null, columnNames));
 
         JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setSize(300, 300);
-        table.setFillsViewportHeight(true);
-        table.setAutoCreateRowSorter(true);
+        //table.setFillsViewportHeight(true);
+        //列排序功能
+        //table.setAutoCreateRowSorter(true);
         //设置表格列不可移动
         table.getTableHeader().setReorderingAllowed(false);
+        //行单选
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
 
 
@@ -58,6 +75,10 @@ public class MGoods {
         jp1.add(deleGoodsButton);
         jp1.add(refreshButton);
         jp2.add(scrollPane);
+        jp3.add(fristPageButton);
+        jp3.add(previousPageButton);
+        jp3.add(nextPageButton);
+        jp3.add(lastPageButton);
 
         mainFrame = new JFrame("商品管理");
         mainFrame.setSize(500,500);
@@ -69,17 +90,48 @@ public class MGoods {
 
         mainFrame.add(jp1);
         mainFrame.add(jp2);
+        mainFrame.add(jp3);
         mainFrame.setVisible(true);
     }
 
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getFristPage() {
+        return fristPage;
+    }
+
+    public void setFristPage(int fristPage) {
+        this.fristPage = fristPage;
+    }
+
+    public int getLastPage() {
+        return lastPage;
+    }
+
+    public void setLastPage(int lastPage) {
+        this.lastPage = lastPage;
+    }
+
     public void go() {
+        setGoodsList();
+
+        initPageNumber();
+
         showData();
+
         refreshButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 showData();
             }
         });
+
         addGoodsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -101,6 +153,7 @@ public class MGoods {
 
                     @Override
                     public void windowClosed(WindowEvent e) {
+                        setCurrentPage(getLastPage());
                         showData();
                     }
 
@@ -213,6 +266,66 @@ public class MGoods {
                 }
             }
         });
+
+        fristPageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setCurrentPage(fristPage);
+                showData();
+            }
+        });
+
+        previousPageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(getCurrentPage() > getFristPage()){
+                    setCurrentPage(currentPage - 1);
+                    showData();
+                }else{
+                    setCurrentPage(getFristPage());
+                    showData();
+                }
+            }
+        });
+
+        nextPageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(getCurrentPage() < getLastPage()){
+                    setCurrentPage(currentPage + 1);
+                    showData();
+                }else{
+                    setCurrentPage(lastPage);
+                    showData();
+                }
+            }
+        });
+
+        lastPageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setCurrentPage(lastPage);
+                showData();
+            }
+        });
+    }
+
+    public void initPageNumber(){
+
+        this.fristPage = 1;
+        this.tableRows = 8;
+        this.currentPage = fristPage;
+
+        if(this.goodsList.size() % this.tableRows ==0){
+            this.lastPage = this.goodsList.size() / this.tableRows;
+        }else{
+            this.lastPage = this.goodsList.size() / this.tableRows + 1;
+        }
+    }
+
+    public void setGoodsList(){
+        GoodsProcess goodsProcess = new GoodsProcess();
+        this.goodsList = goodsProcess.getGoods();
     }
 
     /***
@@ -222,10 +335,11 @@ public class MGoods {
         DefaultTableModel defaultTableModel = (DefaultTableModel) table.getModel();
         defaultTableModel.setRowCount(0);
 
-        GoodsProcess goodsProcess = new GoodsProcess();
-        List<Goods> goodsList = goodsProcess.getGoods();
+        //列表分页
+        Page page = new Page(tableRows);
+        List<Goods> list =  page.cutList(currentPage, goodsList);
 
-        for(Goods goods : goodsList){
+        for(Goods goods : list){
             Vector v = new Vector();
             if (goods.getIsDele() == 0){
                 v.add(goods.getId());
